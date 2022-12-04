e833d675c20047969872e234f2068e86f31e4d7d
vonvic <40438077+vonvic@users.noreply.github.com>
2022-12-04 12:08:04 -0600

    Implement Day 1 Part 2 Solution.

--- 01/problem.py
+++ 01/problem.py
@@ -17,5 +17,15 @@ def part_two_answer() -> int:
     """
     Advent of Code Part One Answer.
     """
-    None
+    with open("01/input.txt", "r") as f:
+        top_three_cal = [0, 0, 0]
+        for calories in f.read().split('\n\n'):
+            cur_sum = sum((int(x) for x in calories.split()))
+            
+            third = min(top_three_cal)
+            third_idx = top_three_cal.index(third)
+            
+            if cur_sum > third:
+                top_three_cal[third_idx] = cur_sum
+    return sum(top_three_cal)
 
