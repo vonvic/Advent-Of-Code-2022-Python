3f9435f969b155d442869d38ad94e5e92ce31f94
vonvic <40438077+vonvic@users.noreply.github.com>
2022-12-04 12:03:53 -0600

    Implement Day 1 Part 1 Solution.

--- 01/problem.py
+++ 01/problem.py
@@ -1,10 +1,16 @@
 """Advent of Code Problem 01 Solution."""
 
+
 def part_one_answer() -> int:
     """
     Advent of Code Part One Answer.
     """
-    None
+    with open("01/input.txt", "r") as f:
+        max_cal = 0
+        for calories in f.read().split('\n\n'):
+            cur_sum = sum((int(x) for x in calories.split()))
+            max_cal = max(max_cal, cur_sum)
+    return max_cal
 
 
 def part_two_answer() -> int:
@@ -12,3 +18,4 @@ def part_two_answer() -> int:
     Advent of Code Part One Answer.
     """
     None
+
