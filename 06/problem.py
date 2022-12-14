aa72d32bcfca3441a4694c0d944d936007ec8284
vonvic <40438077+vonvic@users.noreply.github.com>
2022-12-13 23:22:47 -0600

    Implement Day 06 Part 1 Solution.

--- 06/problem.py
+++ 06/problem.py
@@ -4,7 +4,25 @@ def part_one_answer() -> int:
     """
     Advent of Code Part One Answer.
     """
-    None
+    idx = 0
+    with open("06/input.txt", "r") as f:
+        seq = []
+        while True:
+            c = f.read(1)
+            if not c:
+                break
+            
+            seq.append(c)
+            idx += 1
+            if len(seq) < 4:
+                continue
+            else:
+                if len(set(seq)) == 4:
+                    break
+                else:
+                    seq.pop(0)
+                
+    return idx
 
 
 def part_two_answer() -> int:
