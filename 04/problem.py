e354171942c8811e9acc82de759c762158b3854a
vonvic <40438077+vonvic@users.noreply.github.com>
2022-12-13 22:30:21 -0600

    Implement Day 04 Part 2 Solution.

--- 04/problem.py
+++ 04/problem.py
@@ -28,4 +28,22 @@ def part_two_answer() -> int:
     """
     Advent of Code Part One Answer.
     """
-    None
+    overlap_count = 0
+    with open("04/input.txt", "r") as f:
+        for line in f:
+            pair_assignments = line.strip()
+            fst, snd = pair_assignments.split(',')
+            fst_st, fst_end = fst.split('-')
+            snd_st, snd_end = snd.split('-')
+            fst_st = int(fst_st)
+            fst_end = int(fst_end)
+            snd_st = int(snd_st)
+            snd_end = int(snd_end)
+            
+            if snd_st <= fst_st <= snd_end or\
+                snd_st <= fst_end <= snd_end or\
+                fst_st <= snd_st <= fst_end or\
+                fst_st <= snd_end <= fst_end:
+                overlap_count += 1
+            
+    return overlap_count
