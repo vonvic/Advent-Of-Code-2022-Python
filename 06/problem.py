808a55dad447d97fe20b7805fbf99a3623ad4002
vonvic <40438077+vonvic@users.noreply.github.com>
2022-12-13 23:26:37 -0600

    Implement Day 06 Part 2 Solution.

--- 06/problem.py
+++ 06/problem.py
@@ -1,26 +1,34 @@
 """Advent of Code Problem 06 Solution."""
 
+from io import TextIOWrapper
+
+
+def end_idx_of_uniq_seq(f: TextIOWrapper, length: int) -> int:
+    idx = 0
+    seq = []
+    while True:
+        c = f.read(1)
+        if not c:
+            break
+        
+        seq.append(c)
+        idx += 1
+        if len(seq) < length:
+            continue
+        else:
+            if len(set(seq)) == length:
+                break
+            else:
+                seq.pop(0)
+    return idx
+
 def part_one_answer() -> int:
     """
     Advent of Code Part One Answer.
     """
     idx = 0
     with open("06/input.txt", "r") as f:
-        seq = []
-        while True:
-            c = f.read(1)
-            if not c:
-                break
-            
-            seq.append(c)
-            idx += 1
-            if len(seq) < 4:
-                continue
-            else:
-                if len(set(seq)) == 4:
-                    break
-                else:
-                    seq.pop(0)
+        idx = end_idx_of_uniq_seq(f, 4)
                 
     return idx
 
@@ -29,4 +37,8 @@ def part_two_answer() -> int:
     """
     Advent of Code Part One Answer.
     """
-    None
+    idx = 0
+    with open("06/input.txt", "r") as f:
+        idx = end_idx_of_uniq_seq(f, 14)
+                
+    return idx
