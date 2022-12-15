33287cc641efb04d9e619bbcb406d77e7d4cc1e6
vonvic <40438077+vonvic@users.noreply.github.com>
2022-12-15 14:48:51 -0600

    Implement Day 08 Part 02 Solution.

--- 08/problem.py
+++ 08/problem.py
@@ -1,5 +1,10 @@
 """Advent of Code Problem 08 Solution."""
 
+from collections import namedtuple
+
+Tree = namedtuple("Tuple", ["height", "left", "right", "top", "bottom"])
+
+
 def get_matrix() -> list[list[int]]:
     mat: list[list[int]] = []
     
@@ -7,6 +12,16 @@ def get_matrix() -> list[list[int]]:
         for line in f:
             
             mat.append([int(val) for val in list(line.strip())])
+    
+    return mat
+
+def get_matrix_v2() -> list[list[Tree]]:
+    mat: list[list[int]] = []
+    
+    with open("08/input.txt", "r") as f:
+        for line in f:
+            for val in list(line.strip()):
+                mat.append(Tree(int(val), 0, 0, 0, 0))
     
     return mat
 
@@ -75,6 +90,72 @@ def find_all_visible_from_bottom(mat: list[list[int]]) -> set[tuple[int, int]]:
     
     return visible
 
+
+def get_scenic_score(mat: list[list[Tree]], loc: tuple[int, int]) -> int:
+    left_count = 0
+    right_count = 0
+    top_count = 0
+    bottom_count = 0
+    
+    n = len(mat)
+    m = len(mat[0])
+    
+    i, j = loc
+    height = mat[i][j]
+    
+    # left
+    if j > 0:
+        for x in range(j-1, -1, -1):
+            cur_height = mat[i][x]
+            if height > cur_height:
+                left_count += 1
+            elif height == cur_height:
+                left_count += 1
+                break
+            else:
+                break
+        
+    # right
+    if j < m-1:
+        for x in range(j+1, m):
+            cur_height = mat[i][x]
+            if height > cur_height:
+                right_count += 1
+            elif height == cur_height:
+                right_count += 1
+                break
+            else:
+                break
+    
+    # top
+    if i > 0:
+        for x in range(i-1, -1, -1):
+            cur_height = mat[x][j]
+            if height > cur_height:
+                top_count += 1
+            elif height == cur_height:
+                top_count += 1
+                break
+            else:
+                break
+        
+    # bottom
+    if i < n-1:
+        for x in range(i+1, n):
+            cur_height = mat[x][j]
+            if height > cur_height:
+                bottom_count += 1
+            elif height == cur_height:
+                bottom_count += 1
+                break
+            else:
+                break
+    
+    scenic_score = left_count * right_count * top_count * bottom_count
+    # print(scenic_score)
+    return scenic_score
+
+
 def part_one_answer() -> int:
     """
     Advent of Code Part One Answer.
@@ -94,4 +175,13 @@ def part_two_answer() -> int:
     """
     Advent of Code Part One Answer.
     """
-    None
+    height_map = get_matrix()
+    n = len(height_map)
+    m = len(height_map[0])
+    
+    max_scenic_score = -1
+    for i in range(n):
+        for j in range(m):
+            scenic_score = get_scenic_score(height_map, (i, j))
+            max_scenic_score = max(max_scenic_score, scenic_score)
+    return max_scenic_score
